50181e70290a655e5255ac08703f78fd214d31e2
SNirGH <124217781+SNirGH@users.noreply.github.com>
2023-12-30 10:16:44 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -a
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-set +a
-echo "STARTING INSTALLATION"
-
-(bash $SCRIPT_DIR/scripts/0-preinstall.sh) |& tee 0-preinstall.log
